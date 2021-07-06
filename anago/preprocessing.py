09b43355d21375287fee770bc2e2cc4cfe252fc8
Aditya Rachman Putra <adityarputra@gmail.com>
2021-07-06 22:59:49 +0700

    joblib deprecation

--- anago/preprocessing.py
+++ anago/preprocessing.py
@@ -7,7 +7,7 @@ import re
 import numpy as np
 from allennlp.modules.elmo import Elmo, batch_to_ids
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.externals import joblib
+import joblib
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
